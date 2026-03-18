193197b074a1ff5153595fdf03b06997225c9550
Kristiyan Tsenov <155831101+kr1ss4o@users.noreply.github.com>
2026-03-18 14:37:28 +0200

    AccessDenied.cshtml design
    Added a new design for the page.

--- FoodLoop/obj/Debug/net8.0/FoodLoop.AssemblyInfo.cs
+++ FoodLoop/obj/Debug/net8.0/FoodLoop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodLoop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9361f6ffb86179c7ec54e31dd9cfbe50fa1fbacd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9db25b7f229b9e3d7ac3b75a00e1c9ba7a0a6cd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodLoop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodLoop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
