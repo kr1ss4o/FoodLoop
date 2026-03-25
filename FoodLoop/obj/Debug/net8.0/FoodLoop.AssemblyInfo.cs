9a9fe66d247d2256619f4c47938e65c8cfbba733
Kristiyan Tsenov <155831101+kr1ss4o@users.noreply.github.com>
2026-03-25 10:30:36 +0200

    Responsive Offers/Index page
    The categories carousel now moves only a single category per button swipe, therefore no categories are skipped on lower resolutions.

--- FoodLoop/obj/Debug/net8.0/FoodLoop.AssemblyInfo.cs
+++ FoodLoop/obj/Debug/net8.0/FoodLoop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodLoop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c54506d7312f335e0727fc2711ab63bd5f5af704")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+044b8431dd98ef37a1ceedc4a4e587beac192f46")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodLoop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodLoop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
