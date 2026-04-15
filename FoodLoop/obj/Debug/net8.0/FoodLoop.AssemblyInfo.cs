6a171619171ff85df0c47e3f2c3810ad63768f6a
Kristiyan Tsenov <155831101+kr1ss4o@users.noreply.github.com>
2026-04-15 13:11:55 +0300

    Show logout only for authenticated users
    Wrap the logout forms in Views/Shared/_Navbar.cshtml with an authentication check (User.Identity.IsAuthenticated) for both the navbar icon and sidebar logout. Also simplify the button markup by removing the d-inline/nav-icon-btn classes and tightening the HTML. Other changed files are build artifacts produced by the project build.

--- FoodLoop/obj/Debug/net8.0/FoodLoop.AssemblyInfo.cs
+++ FoodLoop/obj/Debug/net8.0/FoodLoop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodLoop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+542f5a848d2aa2251b0a0752c5de80cb5d620def")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0c9adef79a9053323294d20ec49be0e9c8fbb9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodLoop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodLoop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
